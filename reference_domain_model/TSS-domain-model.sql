6dab09f2c2250e7a6194fa66a0551d8e73353411
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-02-16 22:46:55 +0100

    Assignment 4 graph database astah and population and queries

---
+++ reference_domain_model/TSS-domain-model.sql
@@ -0,0 +1,76 @@
+CREATE TABLE Person (
+ consent BIT(10)
+);
+
+
+CREATE TABLE Role (
+);
+
+
+CREATE TABLE Supervisor (
+);
+
+
+CREATE TABLE Employee (
+);
+
+
+CREATE TABLE Contract (
+ hoursPerWeek:double INT,
+ vacationHours DOUBLE PRECISION,
+ hoursDue DOUBLE PRECISION,
+ workingDaysPerWeek INT,
+ vacationDaysPerYear INT
+);
+
+
+CREATE TABLE ContractStatus (
+);
+
+
+CREATE TABLE Secretary (
+);
+
+
+CREATE TABLE Timesheet (
+ hoursDue DOUBLE PRECISION
+);
+
+
+CREATE TABLE TimesheetEntry (
+ hours DOUBLE PRECISION
+);
+
+
+CREATE TABLE TimesheetFrequency (
+);
+
+
+CREATE TABLE TimesheetStatus (
+);
+
+
+CREATE TABLE Assistant (
+);
+
+
+CREATE TABLE LocalDate (
+);
+
+
+CREATE TABLE LocalTime (
+);
+
+
+CREATE TABLE ReportType (
+);
+
+
+CREATE TABLE String (
+);
+
+
+CREATE TABLE int (
+);
+
+
